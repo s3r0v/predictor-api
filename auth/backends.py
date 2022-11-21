0d9c889def4a310d110b0f35788f292d73424e56
s3r0v <74329180+s3r0v@users.noreply.github.com>
2022-11-21 03:29:07 +0300

    Very effective debug line

--- auth/backends.py
+++ auth/backends.py
@@ -13,6 +13,7 @@ class VerifyRequest(permissions.BasePermission):
         return True
 
     def _checkSignature(self, signature, timestamp, publicKey):
+        print(4)
         pubkey = bytes(PublicKey(publicKey))
         msg = bytes(str(timestamp), 'utf8')
 
@@ -22,20 +23,22 @@ class VerifyRequest(permissions.BasePermission):
             smessage=msg,
             signature=base58.b58decode(signature)
         )
+        print(5)
+        print(result)
         return result
 
     def _checkNFT(self, publicKey):
+        print(2)
         tokens = json.loads(requests.get(SOLSCAN_API_TOKENS + publicKey).text)['data']
         tokenAddress = [token['tokenAddress'] for token in tokens if token['tokenSymbol'] == 'Rev']
         tokenAddress = tokenAddress[0]
         tokenMetadata = json.loads(requests.get(SOLSCAN_API + tokenAddress).text)
-        symbol = tokenMetadata['data']['tokenInfo']['symbol']
         creators = [address['address'] for address in tokenMetadata['data']['metadata']['data']['creators']]
         metadataLink = tokenMetadata['data']['metadata']['data']['uri']
 
         # isActive = json.loads(requests.get(metadataLink).text)['']
         # TODO: isActive
-
+        print(3)
         if CREATOR in creators:
             return True
         return False
@@ -44,6 +47,8 @@ class VerifyRequest(permissions.BasePermission):
         signature = request.GET.get('signature')
         timestamp = request.GET.get('timestamp')
         publicKey = request.GET.get('publicKey')
+
+        print(1)
 
         try:
             if self._checkNFT(publicKey) and self._checkSignature(signature, timestamp,
